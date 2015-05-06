eb88e31144689d77fe305d1599a693da610b62d9
Vitor Britto <code@vitorbritto.com.br>
2015-05-06 15:45:58 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,7 +1,12 @@
 <?php
 
+$vendor_path   = get_template_directory() . '/vendor/';
+require_once $vendor_path . 'podio/api.php';
+
 // Set the API Key:
-define('PODIO_API_KEY', 'YOUR_API_KEY_GOES_HERE');
+define('PODIO_CLIENT_ID', 'YOUR_ID_GOES_HERE');
+define('PODIO_CLIENT_SECRET', 'YOUR_SECRET_GOES_HERE');
+
 
 // Set the App ID:
 define('PODIO_APP_ID', 'YOUR_APP_ID_GOES_HERE');
@@ -20,20 +25,23 @@ $podio_fields = array(
 
 function wp_podio_wrapper($option, $fields) {
 
+    // Set your API Key
+    Podio::setup(PODIO_CLIENT_ID, PODIO_CLIENT_SECRET);
+
     // Authenticate your Application:
-    Podio::authenticate_with_app('PODIO_APP_ID', 'PODIO_APP_TOKEN');
+    Podio::authenticate_with_app(PODIO_APP_ID, PODIO_APP_TOKEN);
 
     // You can create a new item
     switch ($option) {
-    
+
         case 'create':
-            PodioItem::create('PODIO_APP_TOKEN', array('fields' => $fields));
+            PodioItem::create(PODIO_APP_TOKEN, array('fields' => $fields));
             break;
 
         case 'update':
-            PodioItem::update('PODIO_APP_TOKEN', array('fields' => $fields));
+            PodioItem::update(PODIO_APP_TOKEN, array('fields' => $fields));
             break;
-            
+
     }
 }
 
