91a0f562387a70c3365b2f0b60c29f2b7b57c47e
Vitor Britto <code@vitorbritto.com.br>
2015-05-12 23:42:55 -0300

    Add Debug WorkSpace

---
+++ debug/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sandbox for PODIO API Test/Debug</title>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+</body>
+</html>
